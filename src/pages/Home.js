24f9ebe720260fe2dc7e3f6d08abb64b53361858
Asheton S. M <ashetonsm@gmail.com>
2022-11-04 18:02:10 -0500

    CopyOptions() uses mapping to display TextToCopy

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,51 @@
+import { useState } from "react";
 import { FileUpload } from "../components/FileUpload"
+import { TextInput } from "../components/TextInput";
 import { TextToCopy } from "../components/TextToCopy";
 
 export const Home = () => {
+
+    const [copiableText, updateCopiableText] = useState(["Click", "to", "copy"])
+
+    const updateFromPaste = (rawText) => {
+
+        console.log(rawText)
+
+        if (rawText.includes("\n")) {
+            console.log("Contains newlines")
+
+            const splitText = rawText.split("\n")
+
+            console.log(splitText)
+            updateCopiableText(splitText)
+
+        } else {
+            console.log("Does not contain newlines")
+            const singleLine = []
+            singleLine.push(rawText)
+            updateCopiableText(singleLine)
+        }
+    }
+
+    function CopyOptions() {
+
+        var id = 0
+        var output = copiableText.map((props) =>
+            <TextToCopy key={id++}>{props}</TextToCopy>
+        )
+        return output
+    }
 
     return (
         <>
             <div id="sampleText">I will change</div>
             <hr></hr>
-            
-            <TextToCopy>
-                Goodbye, world!
-            </TextToCopy>
 
-            <TextToCopy>
-                Hello, world!
-            </TextToCopy>
+            <CopyOptions />
+            <hr></hr>
 
             <FileUpload />
+            <TextInput updateFromPaste={updateFromPaste} />
         </>
     )
 }
