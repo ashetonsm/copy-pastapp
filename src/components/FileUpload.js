adae4350aac86ca9e3e46096465d758fa440b421
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-11-07 18:32:59 -0600

    Accepts a pdf and returns a base64 blob.

--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -1,9 +1,39 @@
+import * as pdfjs from "pdfjs-dist"
+
+const getDoc = pdfjs.getDocument
+
 export const FileUpload = () => {
+
+    const handleFile = (e) => {
+        e.preventDefault()
+        const file = e.target.files[0]
+
+        if (file.type.toString() !== "application/pdf") {
+            console.log("Invalid filetype")
+        } else {
+            // console.log(file)
+            const reader = new FileReader()
+            reader.readAsDataURL(file)
+            reader.onload = () => {
+                // This is the result without the data:*/*;base64 prefix
+                var result = reader.result.split(",")[1]
+
+                /*
+                var loadingTask = getDoc({ data: result })
+                loadingTask.promise
+                    .then(function (pdf) {
+                        console.log("PDF Loaded")
+                    })
+                */
+            }
+
+        }
+    }
     return (
         <div className="FileUpload">
             <h3>Upload a PDF or DOCX</h3>
             <div>
-                <input type="file" />
+                <input type="file" id="file" onChange={handleFile} />
             </div>
         </div>
     )
