ec1671e3d2a546193ef1a3bdeba5d0af131cb37c
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-11-09 22:33:35 -0600

    Updated to ToggleButtons for feedback on active

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,72 +1,114 @@
 import { useState } from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { ButtonGroup, Col, Container, Row, ToggleButton } from "react-bootstrap";
 import { TextInput } from "../components/TextInput";
-import { TextToCopy } from "../components/TextToCopy";
 
 export const Home = () => {
 
-    const [copiableText, updateCopiableText] = useState(["Click", "to", "copy"])
+    const [radioValue, setRadioValue] = useState(0)
+    const [copiableText, updateCopiableText] = useState([
 
-    const updateFromPaste = (inputArray) => {
+        { text: 'Click', value: 0 },
+        { text: 'to', value: 1 },
+        { text: 'Copy', value: 2 },
 
-        console.log(inputArray.length)
+    ])
 
-        if (inputArray.length > 1) {
+    const updateFromPaste = (inputObj) => {
+        setRadioValue(0)
+        console.log(inputObj)
+        console.log(inputObj.length)
+
+        if (inputObj.length > 1) {
             console.log("Contains multiple lines")
 
-            updateCopiableText(inputArray)
+            updateCopiableText(inputObj)
 
         } else {
             console.log("Does not contain multiple lines")
-            updateCopiableText(inputArray)
+            updateCopiableText(inputObj)
         }
+    }
+
+
+    const copyText = (newText) => {
+
+        console.log(newText)
+
+        return (
+            navigator.clipboard
+                .writeText(newText)
+                .then(() => (document.querySelector("#currentText").innerText = newText))
+        )
     }
 
     function CopyOptions() {
 
-        var id = 0
-        var output = copiableText.map((props) =>
-            <TextToCopy key={id++} notify={notify}>{props}</TextToCopy>
-        )
-        return output
-    }
+        var output = copiableText.map((radio, idx) =>
 
-    const notify = () => {
-        console.log("Toasty")
+            <p>
+                <ToggleButton
+                    key={idx}
+                    id={`radio-${idx}`}
+                    type="checkbox"
+                    name="radio"
+                    value={radio.value}
+                    checked={radioValue === radio.value}
+                    onChange={(e) => {
+                        setRadioValue(parseInt(e.currentTarget.value))
+                        copyText(radio.text)
+                    }}
+                >
+                    {radio.text}
+                </ToggleButton>
+            </p>
+        )
+        return (
+            <Col>
+                <ButtonGroup vertical>
+                    {output}
+                </ButtonGroup>
+            </Col>
+        )
     }
 
     return (
         <Container>
+            <Container>
 
-            {/* Centered title */}
-            <Row className="row justify-content-md-center">
-                <Col className="col-lg-auto">
-                    <h1>Copy-Pastapp</h1>
-                    <h2>(Because auto-fill isn't always accurate)</h2>
-                </Col>
-            </Row>
+                {/* Centered title */}
+                <Row className="row justify-content-md-center">
+                    <Col className="col-lg-auto">
+                        <h1 style={{ textAlign: 'center' }}>Copy-Pastapp</h1>
+                        <h2 style={{ textAlign: 'center' }}>(Because auto-fill isn't always accurate)</h2>
+                    </Col>
+                </Row>
 
-            <hr></hr>
+                <hr></hr>
 
-            {/* Text Input Area */}
-            <Row className="row justify-content-md-center">
-                <Col className="col-lg-auto">
+                {/* Text Input Area */}
+                <Row className="row justify-content-md-center">
+                    <Col className="col-lg-auto">
 
-                    <TextInput updateFromPaste={updateFromPaste} />
-                </Col>
-            </Row>
+                        <TextInput updateFromPaste={updateFromPaste} />
+                    </Col>
+                </Row>
+            </Container>
 
             {/* Results */}
-            <Row className="row justify-content-md-center">
-                <Col>
-                    <div id="currentText">I will change</div>
+            <Container fluid="lg" className="overflow-auto" style={{ height: '50vh' }}>
+                <Row className="row justify-content-md-center">
+                    <Col className="col-lg-auto">
 
-                    <div className="h-20 overflow-auto" style={{ height: '50vh' }}>
-                        <CopyOptions />
-                    </div>
-                </Col>
-            </Row>
+                        <h3 id="currentText">This is the currently copied text...</h3>
+                    </Col>
+                </Row>
+
+                <Row className="row justify-content-lg-center">
+                    <CopyOptions />
+                </Row>
+            </Container>
 
         </Container>
+
     )
 }
