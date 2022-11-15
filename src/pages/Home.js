9bd3fc38698232d7a23b81de14717eaf9ddb45e5
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-11-15 10:47:23 -0600

    Moved fx related to list gen into CopiableText

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,13 @@
 import { useState } from "react";
-import { ButtonGroup, Col, Container, Row, ToggleButton } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
+import { CopiableText } from "../components/CopiableText";
 import { TextInput } from "../components/TextInput";
 
 export const Home = () => {
 
     const [selectedValue, setSelectedValue] = useState(0)
     const [copyValue, setCopyValue] = useState(0)
-    const [appendingValue, setAppendingValue] = useState(null)
-    const [appendingText, setAppendingText] = useState("")
+
     const [copiableText, setCopiableText] = useState([
 
         { text: 'Click', value: 0, bullet: false },
@@ -19,101 +19,6 @@ export const Home = () => {
     const updateFromPaste = (inputObj) => {
         setCopyValue(0)
         setCopiableText(applyFormatting(selectedValue, inputObj))
-    }
-
-    const removeElement = (oldObj) => {
-        var oldValue = oldObj.id
-        var newArray = []
-
-        // console.log(`DELETING`)
-        newArray = copiableText.filter((elem) => elem !== oldObj)
-
-        try {
-            if (copiableText[oldValue].id !== undefined) {
-                // console.log(copiableText[oldValue].value)
-            }
-        } catch (error) {
-            console.log("WARNING: Undefined value for old object!")
-        }
-
-        // console.log(newArray)
-
-        setCopiableText(newArray)
-    }
-
-    const appendElement = (destinationObj, destinationID, appendingID) => {
-
-        if (destinationID !== parseInt(appendingID)) {
-            // console.log(destinationID  + " " + appendingID)
-            console.log(`Appending "${appendingText}" onto "${destinationObj.text}".`)
-
-            copiableText[destinationID].id = appendingID
-
-            copiableText[destinationID].text = copiableText[destinationID].text.concat(" " + appendingText)
-
-            removeElement(copiableText[appendingID])
-
-        } else {
-            console.log("Aborting append operation.")
-        }
-    }
-
-    const copyText = (newText) => {
-        return (
-            navigator.clipboard
-                .writeText(newText)
-                .then(() => (document.querySelector("#currentText").innerText = newText))
-        )
-    }
-
-    function CopyOptions() {
-        var output = copiableText.map((radio, idx) =>
-
-            <p key={idx}
-                onDragStart={(e) => {
-                    setAppendingText(radio.text)
-                    setAppendingValue(parseInt(e.currentTarget.children[1].value))
-                    // console.log(`Dragging "${radio.text}"... Value: ${e.currentTarget.children[1].value}`)
-                }}
-                onDragOver={(e) => {
-                    e.preventDefault()
-                }}
-                onDrop={(e) => {
-                    // console.log(`Dropped #${appendingValue} on #${e.currentTarget.children[1].value}`)
-                    appendElement(radio, parseInt(e.currentTarget.children[1].value), parseInt(appendingValue))
-                }}
-                style={{ padding: "1em" }}
-                draggable
-            >
-                <span style={{ marginRight: "1em", cursor: "grab" }}>📄</span>
-
-                <ToggleButton
-                    id={`radio-${idx}`}
-                    type="checkbox"
-                    name="radio"
-                    value={idx}
-                    checked={copyValue === idx}
-                    onChange={(e) => {
-                        setCopyValue(parseInt(e.currentTarget.value))
-                        copyText(radio.text)
-                    }}
-                >
-                    {radio.text}
-                </ToggleButton>
-                <span onClick={(e) => {
-                    // console.log(radio)
-                    // console.log(e.target.parentNode.children[1].value)
-                    removeElement(radio)
-                }} style={{ marginLeft: "1em", cursor: "default" }}>❌</span>
-            </p>
-        )
-        return (
-            <Col>
-                <ButtonGroup vertical>
-                    {output}
-                </ButtonGroup>
-            </Col>
-        )
     }
 
     const applyFormatting = (optionNum, inputObj) => {
@@ -221,7 +126,12 @@ export const Home = () => {
             <Container fluid="lg" className="overflow-auto" style={{ height: '50vh' }}>
 
                 <Row className="row justify-content-lg-center">
-                    <CopyOptions />
+                    <CopiableText
+                        copiableText={copiableText}
+                        functions={{ setCopiableText, setCopyValue }}
+                        copyValue={copyValue}
+
+                    />
                 </Row>
             </Container>
 
