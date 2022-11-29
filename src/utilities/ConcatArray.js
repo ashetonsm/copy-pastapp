a9877b96b44a9c40e53d83bf9f18b36259a89ad6
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-11-29 11:55:01 -0600

    Removed log and extra breaks.

--- src/utilities/ConcatArray.js
+++ src/utilities/ConcatArray.js
@@ -1,10 +1,7 @@
 export const ConcatArray = (inputList) => {
-    console.log(inputList)
     var concatList = ""
-
     inputList.forEach(item => {
         concatList = concatList.concat(item.text + "\n")
     })
-
     return concatList
 }
