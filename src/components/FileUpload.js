27c7a61fc5db4d5108cdb68bfd4b9c24c5a9febb
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-12-08 21:38:52 -0600

    Deleted old code.

--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -1,33 +1,7 @@
 import { PDFTest } from "./PDFTest"
 
 export const FileUpload = () => {
-
-    const handleFile = (e) => {
-        e.preventDefault()
-        const file = e.target.files[0]
-
-        if (file.type.toString() !== "application/pdf") {
-            console.log("Invalid filetype")
-        } else {
-            console.log(file)
-            const reader = new FileReader()
-            reader.readAsDataURL(file)
-            reader.onload = () => {
-                // This is the result without the data:*/*;base64 prefix
-                var result = reader.result.split(",")[1]
-
-                console.log(result)
-            }
-
-        }
-    }
     return (
-        <div className="FileUpload">
-            <h3>Upload a PDF or DOCX</h3>
-            <div>
-                <input type="file" id="file" onChange={handleFile} />
-            </div>
-            <PDFTest/>
-        </div>
+        <PDFTest />
     )
 }
