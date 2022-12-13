f58bce4f6436ba636849fe36d557b1409662995a
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-12-13 00:32:09 -0600

    Basic text file reader.

--- src/components/ReadTXT.js
+++ src/components/ReadTXT.js
@@ -1,13 +1,18 @@
-export const ReadTXT = ({file}) => {
+import { useContext, useEffect } from 'react';
 
-    const parseTxt = (txtFile) => {
-        if (txtFile !== null && txtFile.type === "text/plain") {
-            console.log(file.name + " is a .txt file.")
+import TextInputContext from "../context/TextInputContext"
+
+export const ReadTXT = ({ file }) => {
+
+    const { dispatch } = useContext(TextInputContext)
+
+    useEffect(() => {
+        const fileReader = new FileReader()
+        fileReader.readAsText(file)
+        fileReader.onload = () => {
+            var rawText = fileReader.result
+            return dispatch({ type: 'SET_LOADED_INPUT', payload: rawText })
         }
-    }
-    
-    return (
-        parseTxt(file)
-        // file.type === "text/pdf" ? console.log("txt file") : null
-    )
+
+    }, [file, dispatch])
 }
